b5611f83419f113b5ef314dcc43f8bff2dc4582d
mcornielly <mcornielly@gmail.com>
2022-07-18 23:45:10 -0400

    adminLTE 3 Installation completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,26 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <div class="card">
+        <div class="card-header">
+            <h2 class="card-title">Titulo de prueba</h2>
+        </div>
+        <div class="card-body">
+            <p>Welcome to this beautiful admin panel.</p>
         </div>
     </div>
-</div>
-@endsection
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
